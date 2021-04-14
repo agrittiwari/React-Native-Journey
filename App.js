34a0a383dc05f94caabc3d119d4fcea3b64ef0e8
Agrit Tiwari <agrit1408@gmail.com>
2021-04-14 18:01:20 +0530

    ListItem syntax got deprecated, need to follow up

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Main from './Component/MainComponent'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Started working my app</Text>
-      <StatusBar style="auto" />
-    </View>
+     
+      <Main />
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Component/MainComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Menu from './MenuComponent'
+import { DISHES } from '../Shared/dishes'
+import { View } from 'react-native'
+
+class Main extends Component {
+    constructor(props)
+    {
+        super()
+        this.state = {
+            dishes: DISHES
+        }
+}
+
+    render(){
+        return (
+            <View>
+                    <Menu dishes={this.state.dishes} />
+            </View>
+            
+        )
+        
+    }
+
+}
+
+export default Main;
